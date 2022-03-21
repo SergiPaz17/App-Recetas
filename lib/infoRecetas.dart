181f9f7028823937d2361d78f419f5a1766328a4
ItsHaru7 <callmeharu7@gmail.com>
2022-03-21 20:12:24 +0100

    Seguir con el carrusel de imagenes

--- lib/infoRecetas.dart
+++ lib/infoRecetas.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
@@ -16,6 +19,7 @@ class InfoRecetas extends StatefulWidget {
 class _InformacionRecetas extends State<InfoRecetas> {
   List _items = [];
   List _ingredientes = [];
+  List _pasos = [];
   // Fetch content from the json file
   Future<void> readJson() async {
     final String response =
@@ -25,6 +29,7 @@ class _InformacionRecetas extends State<InfoRecetas> {
     setState(() {
       _items = data["items"];
       _ingredientes = data["items"][widget.indexRecetas]["ingredientes"];
+      _pasos = data["items"][widget.indexRecetas]["pasos"];
     });
   }
 
@@ -37,37 +42,131 @@ class _InformacionRecetas extends State<InfoRecetas> {
         backgroundColor: Colors.black45,
         title: Text(title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(1),
-        child: Column(
-          children: [
-            Image.asset(_items[widget.indexRecetas]["Imagen"]),
-            const Divider(),
-            const Text(
-              "Ingredientes",
-              textScaleFactor: (2),
-            ),
-            const Divider(),
-            _ingredientes.isNotEmpty
-                ? Expanded(
-                    child: ListView.builder(
-                    itemCount: _ingredientes.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: Column(children: [
-                          ListTile(
-                            title: Text(_ingredientes[index]["Nombre"]),
-                            subtitle: Text(_ingredientes[index]["Cantidad"]),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(2),
+          child: Column(
+            children: [
+              Image.asset(_items[widget.indexRecetas]["Imagen"]),
+              const Divider(),
+              const Text(
+                "Ingredientes",
+                textScaleFactor: (2),
+              ),
+              const Divider(),
+              _ingredientes.isNotEmpty
+                  ? ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _ingredientes.length,
+                      shrinkWrap: true,
+                      itemExtent: 40,
+                      itemBuilder: (context, index) {
+                        return Row(children: [
+                          Text(_ingredientes[index]["Nombre"] + ": "),
+                          Text(_ingredientes[index]["Cantidad"] + " "),
+                          const Spacer(),
+                          const Icon(Icons.shopping_cart),
+                        ]);
+                      },
+                    )
+                  : Container(),
+              const Divider(),
+              const Text(
+                "Pasos",
+                textScaleFactor: (2),
+              ),
+              const Divider(),
+              _pasos.isNotEmpty
+                  ? ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _pasos.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return Column(children: [
+                          Row(
+                            children: [
+                              Text(
+                                "Paso " + index.toString(),
+                                textScaleFactor: 1.2,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                        ]),
-                      );
-                    },
-                  ))
-                : Container(),
-          ],
+                          const Divider(
+                            thickness: 2,
+                          ),
+                          Text(_pasos[index]["paso"]),
+                          const Text(""),
+                        ]);
+                      },
+                    )
+                  : ListView.builder(
+                      itemCount: _items.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          margin: const EdgeInsets.all(25),
+                          child: InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => InfoRecetas(index),
+                                  ),
+                                );
+                              },
+                              child: Column(children: [
+                                ListTile(
+                                  title: Text(_items[index]["nombre"]),
+                                  subtitle: Text(_items[index]["descripcion"]),
+                                ),
+                                Column(
+                                  children: [
+                                    Image.asset(_items[index]["Imagen"]),
+                                  ],
+                                )
+                              ])),
+                        );
+                      },
+                    ),
+              Container(
+                height: 500,
+                child: ListView.builder(
+                  itemCount: _items.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      margin: const EdgeInsets.all(25),
+                      child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => InfoRecetas(index),
+                              ),
+                            );
+                          },
+                          child: Column(children: [
+                            ListTile(
+                              title: Text(_items[index]["nombre"]),
+                              subtitle: Text(_items[index]["descripcion"]),
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(_items[index]["Imagen"]),
+                              ],
+                            )
+                          ])),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
